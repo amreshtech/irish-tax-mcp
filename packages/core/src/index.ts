6a1ed14a2c3c451bac83d4b56adfd19743961cf2
amreshtech <amreshtech@gmail.com>
2026-05-11 03:04:45 +0100

    feat: export deterministic calculator interfaces

---
+++ packages/core/src/index.ts
@@ -0,0 +1,18 @@
+export { calculateIncomeTax } from './income-tax.js';
+export { calculateVat } from './vat.js';
+export { calculateCgt } from './cgt.js';
+export type {
+  TaxRates,
+  IncomeTaxParams,
+  IncomeTaxResult,
+  VatParams,
+  VatResult,
+  CgtParams,
+  CgtResult,
+  FilingStatus,
+  PrsiClass,
+  VatCode,
+  VatDirection,
+  TaxCreditKey,
+  TaxBand,
+} from './types.js';
